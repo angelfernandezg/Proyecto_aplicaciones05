00b2cdabc0c700fcd10dc2b99a8f7b754c468913
IsmaBona <113512126+IsmaBona@users.noreply.github.com>
2022-11-12 18:35:00 +0100

    Add files via upload

--- Proyecto_Aplicaciones05/src/Objetos/Sala3.java
+++ Proyecto_Aplicaciones05/src/Objetos/Sala3.java
@@ -1,6 +1,8 @@
 package Objetos;
 
-public class Sala3 extends Cofre{
+import java.util.Scanner;
+
+public class Sala3 extends Cofre implements inspeccionar{
 	protected int cofre;
 	protected int llave;
 	protected int capa;
@@ -27,15 +29,37 @@ public class Sala3 extends Cofre{
 		}
 		else {
 			System.out.println("Parece que el cofre no se encuentra por aquí");
+		}
+	}
+		
+		public void getInspecionarsala(inspeccionar I) {
+			
+			Scanner scanner = new Scanner(System.in);
+			String respuesta = scanner.nextLine();
+			scanner.close();
+			if (respuesta == "i" | respuesta == "inspeccionar"){
+				if (objeto == 1) {
+					System.out.println("Has encontrado la capa de superman, di equipar para lucirla");
+					Scanner scanner2 = new Scanner(System.in);
+					String respuesta2 = scanner2.nextLine();
+					scanner2.close();
+					if (respuesta2 == "equipar") {
+						System.out.println("¡Qué bien queda me la voy a quedar! Aparte seguro que da fps extra.");
+					}
+				}
+				
+				if (objeto == 1) {
+					System.out.println("Has encontrado la espada de el maestro jedi Yoda  , di equipar para poder utilizarlo");
+					Scanner scanner3 = new Scanner(System.in);
+					String respuesta3 = scanner3.nextLine();
+					scanner3.close();
+					if (respuesta3 == "equipar") {
+						System.out.println("Parece muy inestable, mejor la guardo y ya aprenderé a usarla\nmás adelante,"
+								+ "cuando no halla tanto peligro.");
+					}
+				}
 			}
-		
-		if (objeto == 1) {
-			System.out.println("Has encontrado la capa de superman, di equipar para lucirla");
-		}
-		
-		if (objeto == 1) {
-			System.out.println("Has encontrado la espada de el maestro jedi Yoda  , di equipar para poder utilizarlo");
-		}
+				
 	}
 	
 }
--- Proyecto_Aplicaciones05/src/Objetos/Sala4.java
+++ Proyecto_Aplicaciones05/src/Objetos/Sala4.java
@@ -1,5 +1,7 @@
 package Objetos;
 
+import java.util.Scanner;
+
 public class Sala4 extends Cofre{
 	protected int cofre;
 	protected int llave;
@@ -27,14 +29,38 @@ public class Sala4 extends Cofre{
 		else {
 			System.out.println("Parece que el cofre no está por aquí");
 			}
+	}
 		
-		if (objeto == 1) {
-			System.out.println("Has encontrado el libro de la leyenda de Troya, di abrir para leerlo");
-		}
 		
-		if (objeto == 1) {
-			System.out.println("Has encontrado los guante de Freddy Krueger , di equipar para poder utilizarlo");
-		}
+		public void getInspecionarsala(inspeccionar I) {
+			
+			Scanner scanner = new Scanner(System.in);
+			String respuesta = scanner.nextLine();
+			scanner.close();
+			if (respuesta == "i" | respuesta == "inspeccionar"){
+				if (objeto == 1) {
+					System.out.println("Has encontrado el libro de la leyenda de Troya, di abrir para leerlo");
+					Scanner scanner2 = new Scanner(System.in);
+					String respuesta2 = scanner2.nextLine();
+					scanner2.close();
+					if (respuesta2 == "leer") {
+						System.out.println("Parece que como conquistaron los troyanos este libro esta "
+								+ "escondido bajo un caballo. Ya averiguaremos lo entenderemos");
+					}
+				}
+				
+				if (objeto == 1) {
+					System.out.println("Has encontrado los guante de Freddy Krueger , di equipar para poder utilizarlo");
+					Scanner scanner3 = new Scanner(System.in);
+					String respuesta3 = scanner3.nextLine();
+					scanner3.close();
+					if (respuesta3 == "equipar") {
+						System.out.println("Parecen peligrosas. ¡Qué bien ya no se me acercarán");
+					}
+				}
+			}
+		
+		
 	}
 	
 }
