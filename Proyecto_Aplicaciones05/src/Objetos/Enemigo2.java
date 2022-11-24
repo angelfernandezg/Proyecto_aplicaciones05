f678f44d59f57842fb3bc8d290d53a4345eb70c5
oierg <oierg@LAPTOP-8QLPO0T6>
2022-11-24 12:05:09 +0100

    enemigosss

--- Proyecto_Aplicaciones05/src/Objetos/Enemigo2.java
+++ Proyecto_Aplicaciones05/src/Objetos/Enemigo2.java
@@ -3,11 +3,25 @@ package Objetos;
 public class Enemigo2 extends Enemigo{
 
 //Esqueleto
-	protected int espada;
+	protected double espada = 1.3;
 
 	public Enemigo2(Boolean tieneLlave, int objeto, int salud, int ataque, int defensa, int experiencia, int espada) {
-		super(tieneLlave, objeto, salud, ataque, defensa, experiencia);
+		super(salud, ataque, defensa, experiencia);
 		this.espada = espada;
 	}
 	
+	public double getEspada() {
+		return espada;
+	}
+
+	public void setEspada(double espada) {
+		this.espada = espada;
+	}
+
+	public void defender() {
+		double recib2 = ;
+	}
+	public void atacar() {
+		double ataq2 = this.ataque*this.espada;
+	}
 }
