1957f04c97cefec15f914521071f5c728a8d8c26
oierg <oierg@LAPTOP-8QLPO0T6>
2022-12-28 19:29:35 +0100

    Inventario ventana

--- Proyecto_Aplicaciones05/src/Ventanas/Inventario.java
+++ Proyecto_Aplicaciones05/src/Ventanas/Inventario.java
@@ -1,5 +1,7 @@
 package Ventanas;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -11,21 +13,19 @@ public class Inventario extends JFrame{
 
 	public Inventario() {
 		
+		setTitle("Inventario");
+		this.setSize(1200,600);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 		String[] columnas = {"Objeto", "Clase", "Cantidad"};
 		
 		Object[][] datos = {};
 		
 		getContentPane().setLayout(getLayout());
 		JTable tablainventario = new JTable(datos, columnas);
-		
-		JScrollPane scrollinventario = new JScrollPane(tablainventario);
-		
-		getContentPane().add(scrollinventario);
-		
-		this.setLocationRelativeTo(null);
-		this.setSize(1200,600);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setVisible(false);
+		add(new JScrollPane(tablainventario), BorderLayout.CENTER);
+		getContentPane().add(scrollinventario);	
 	}
 }
 
